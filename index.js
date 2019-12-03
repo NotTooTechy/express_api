8c8da5c5ec6a84becadf60dd54015458bcb1b7ad
NotTooTechy <NotTooTechy@mail.com>
2019-12-03 11:58:28 -0500

    Added handlebars, and used bootstrap. Created preaty view of members json

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 const express = require('express');
 const path = require('path');
+const exphbs = require('express-handlebars');
 const app = express();
 const logger = require('./middleware/logger');
+const members_dict = require('./routes/api/members_dict.js')
 
 const PORT = process.env.PORT || 5000;
 
 // Initilize middleware
 app.use(logger);
 
+// Initilize Handlebars middleware, as per documentation..
+app.engine('handlebars', exphbs({defaultLayout : 'main'}));
+app.set('view engine', 'handlebars');
+
 // Body Parser Middleware
 app.use(express.json()); // handle raw json
 app.use(express.urlencoded({extended: false})); // handle url encoded data
+
+// Homepage view
+app.get('/', (req, res) => res.render('index', {
+	title: 'Member App',
+	members_dict // the same as .. members: members
+}));
 
 // static folder
 app.use(express.static(path.join(__dirname, 'public')));
@@ -18,9 +30,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 // importing members
 app.use('/api/members', require('./routes/api/members'));
 // Static home page
-app.get('/', (req, res) => {
-	res.send('<h2>Homepage</h2>');
-});
+//app.get('/', (req, res) => {
+//	res.send('<h2>Homepage</h2>');
+//});
 
 
 
